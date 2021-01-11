46247f998425686936444862d11115a7a68d358c
smatalha <sma.talha@gmail.com>
2021-01-10 22:02:55 -0500

    edit about

--- portfolio-app/src/components/About.js
+++ portfolio-app/src/components/About.js
@@ -1,10 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class  extends Component {
-    state = {  }
-    render() { 
-        return (  );
+class About extends React.Component {
+
+    render() {
+        return (
+            <section>
+                <div className="about">
+                <h2>What even is this place?</h2>
+                    <p>EventsInc is a personal organization and planning tool enabling users to explore upcoming events, keep track of their favorites, and even add their own events to a collection of public listings. In addition to picking out your future adventures and dreaming of the outside world, you can use our to-do list to map out all the things you need to make those dreams a reality.</p>
+                    <p>A bachelor trip to Tulum, the best band you'd never heard of playing live in your hood, or outdoor game night in the park with friends, on EventsInc you can explore, plan, and share to your heart's content.</p>
+
+                </div>
+
+            </section>
+
+        );
     }
+
 }
- 
-export default ;
+
+export default About;
+
