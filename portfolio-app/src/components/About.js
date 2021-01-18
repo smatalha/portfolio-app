0cbbccfe806fb1c6ad575e38a77f247b46b47d00
smatalha <sma.talha@gmail.com>
2021-01-17 23:24:36 -0500

    About edited

--- portfolio-app/src/components/About.js
+++ portfolio-app/src/components/About.js
@@ -7,9 +7,6 @@ class About extends React.Component {
             <section>
                 <div className="about">
                 <h2>What even is this place?</h2>
-                    <p>EventsInc is a personal organization and planning tool enabling users to explore upcoming events, keep track of their favorites, and even add their own events to a collection of public listings. In addition to picking out your future adventures and dreaming of the outside world, you can use our to-do list to map out all the things you need to make those dreams a reality.</p>
-                    <p>A bachelor trip to Tulum, the best band you'd never heard of playing live in your hood, or outdoor game night in the park with friends, on EventsInc you can explore, plan, and share to your heart's content.</p>
-
                 </div>
 
             </section>
